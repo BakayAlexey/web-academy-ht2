66d00070dd357f9c7901e9b2dc96d89c7b181407
Bakay Alexey <bakaylexa@gmail.com>
2019-11-28 23:56:14 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const typesClock = [
+  'hms', 'mdY', 'Mdy', 'hm'
+];
+
+const month = [
+  'January ',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+class App extends Component {
+  state = {
+    time: new Date(),
+    currentType: 0,
+  };
+
+  componentDidMount() {
+    setInterval(this.update, 1000);
+  }
+
+  update = () => {
+    this.setState({
+      time: new Date(),
+    });
+  };
+
+  getClock = () => {
+    const { time, currentType } = this.state;
+
+    switch (typesClock[currentType]) {
+      case 'hms': {
+        const h = time.getHours();
+        const m = time.getMinutes();
+        const s = time.getSeconds();
+
+        return (
+          <div className='container hms'>
+            {(h > 12) ? (h % 12) : h} : {(`0${m}`).slice(-2)} : {(`0${s}`).slice(-2)} {(h < 12) ? 'AM' : 'PM'}
+          </div>
+        );
+      }
+
+      case 'mdY': {
+        const m = time.getMonth() + 1;
+        const d = time.getDate();
+        const Y = time.getFullYear();
+
+        return (
+          <div className='container mdY'>
+            {m}/{d}/{Y}
+          </div>
+        );
+      }
+
+      case "Mdy": {
+        const M = month[time.getMonth()];
+        const d = time.getDate();
+        const y = time.getFullYear().toString().slice(-2);
+
+        return (
+          <div className='container Mdy'>
+            {M} {d} {y}
+          </div>
+        );
+      }
+
+      case 'hm': {
+        const h = time.getHours();
+        const m = time.getMinutes();
+
+        return (
+          <div className='container hm'>
+            {(h > 12) ? (h % 12) : h} : {(`0${m}`).slice(-2)} {(h < 12) ? 'AM' : 'PM'}
+          </div>
+        );
+      }
+    }
+  };
+
+  toggleTypeClock = () => {
+    this.setState(({currentType}) => ({
+      currentType: (currentType + 1) % typesClock.length,
+    }));
+  };
+
+  render() {
+    return (
+      <div onClick={this.toggleTypeClock}>
+        {this.getClock()}
+      </div>
+    );
+  }
 }
 
 export default App;
